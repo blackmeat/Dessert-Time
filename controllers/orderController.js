a14c9acf0f0a8b486fc600d8aaefdd5342db0a49
Henry Wong <joe8632782@gmail.com>
2020-05-13 20:35:26 +0800

    Add decrypt function in orderController file, According spagatewayCallback update payment_status column

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -17,7 +17,7 @@ const transporter = nodemailer.createTransport({
     refreshToken: process.env.REFRESHTOKEN,
   }
 });
-const URL = "https://d866fd3b.ngrok.io"
+const URL = ""
 const MerchantID = process.env.MERCHANTID
 const HashKey = process.env.HASHKEY
 const HashIV = process.env.HASHIV
@@ -40,12 +40,20 @@ function create_mpg_aes_encrypt(TradeInfo) {
   return enc + encrypt.final("hex");
 }
 
-function getTradeInfo(Amt, Desc, email) {
+function create_mpg_aes_decrypt(TradeInfo) {
+  let decrypt = crypto.createDecipheriv("aes256", HashKey, HashIV);
+  decrypt.setAutoPadding(false);
+  let text = decrypt.update(TradeInfo, "hex", "utf8");
+  let plainText = text + decrypt.final("utf8");
+  let result = plainText.replace(/[\x00-\x20]+/g, "");
+  return result;
+}
+
+function getTradeInfo(Amt, Desc, email, ) {
 
   console.log('===== getTradeInfo =====')
   console.log(Amt, Desc, email)
   console.log('==========')
-
   data = {
     'RespondType': 'JSON', // 回傳格式
     'TimeStamp': Date.now(), // 時間戳記
@@ -56,7 +64,7 @@ function getTradeInfo(Amt, Desc, email) {
     'PeriodType': 'M', // 交易週期
     'PeriodPoint': 14, // 交易週期時間點
     'PeriodStartType': 2, // 檢查卡號模式（共三種）
-    'PeriodTimes': 24, // 交易期數
+    'PeriodTimes': 12, // 交易期數
     'PayerEmail': email, // 付款人電子信箱
     'ReturnURL': ReturnURL, // 支付完成返回商店網址
     'NotifyURL': NotifyURL, // 支付通知網址/每期授權結果通知
@@ -126,15 +134,56 @@ const orderController = {
       .findByPk(req.params.id, { include: Product })
       .then(order => {
         const tradeInfo = getTradeInfo(order.amount, order.Product.name, order.subscriber_email)
-        res.render("checkout", { order, tradeInfo })
+        if (order.payment_status === "尚未付款" || order.payment_status === "付款失敗") {
+          order.update({
+            ...order,
+            sn: tradeInfo.MerchantOrderNo
+          }).then(order => {
+            res.render("checkout", { order, tradeInfo })
+          })
+        } else {
+          res.render("checkout", { order, tradeInfo })
+        }
+
       })
   },
   spgatewayCallback: (req, res) => {
     console.log('===== spgatewayCallback =====')
+    console.log(req.method)
     console.log(req.body)
     console.log('==========')
 
-    return res.redirect('back')
+    const data = JSON.parse(create_mpg_aes_decrypt(req.body.Period))
+
+    console.log('===== spgatewayCallback: create_mpg_aes_decrypt、data =====')
+    console.log(data)
+
+    if (data.Status === "SUCCESS") {
+      Order
+        .findOne({ where: { sn: data.Result.MerchantOrderNo } })
+        .then(order => {
+          order.update({
+            ...order,
+            payment_status: "完成付款",
+          }).then(order => {
+            order.save()
+            return res.redirect(`/order/${order.id}/checkout`)
+          })
+        })
+    } else {
+      Order
+        .findOne({ where: { sn: data.Result.MerchantOrderNo } })
+        .then(order => {
+          order.update({
+            ...order,
+            payment_status: "付款失敗",
+          }).then(order => {
+            order.save()
+            return res.redirect(`/order/${order.id}/checkout`)
+          })
+        })
+    }
+
   }
 }
 
