a53584144ddc2cc0cd6e4a510225ddbff47d576a
Henry Wong <joe8632782@gmail.com>
2020-05-16 11:56:57 +0800

    Update POST /signup controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,22 +16,23 @@ const userController = {
   signUp: (req, res) => {
     if (req.body.password !== req.body.passwordCheck) {
       req.flash("error_messages", "兩次密碼輸入不相同")
-      return res.redirect("/signup")
+      return res.redirect("/my-account")
     } else {
       User
         .findOne({ where: { email: req.body.email } })
         .then((user) => {
           if (user) {
             req.flash("error_messages", "信箱已經被註冊過")
-            return res.redirect("/signup")
+            return res.redirect("/my-account")
           } else {
             User.create({
               name: req.body.name,
               email: req.body.email,
               password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
+              role: false
             }).then(user => {
               req.flash("success_messages", "成功註冊！！")
-              return res.redirect('/signin')
+              return res.redirect("/my-account")
             })
           }
         })
