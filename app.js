697288ad0467bc234b89e70c396e613f7b5ff7d2
Henry Wong <joe8632782@gmail.com>
2020-05-19 12:18:10 +0800

    Update Port in app file

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require("express-session")
 const flash = require("connect-flash")
 const methodOverride = require("method-override")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config()
