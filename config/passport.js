08e8f6e379fc2ca96bbc6018390c80eae3c530ec
Henry Wong <joe8632782@gmail.com>
2020-06-02 22:44:10 +0800

    Install bcryptjs, Uninstall bcrypt-nodejs

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require("passport")
 const LocalStrategy = require("passport-local").Strategy
-const bcrypt = require("bcrypt-nodejs")
+const bcrypt = require("bcryptjs")
 const db = require("../models")
 const User = db.User
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt-nodejs")
+const bcrypt = require("bcryptjs")
 const helpers = require("../helpers")
 const db = require("../models")
 const User = db.User
